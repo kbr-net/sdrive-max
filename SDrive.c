5f7ccc20365fc029e63431785b9c19649df26f4b
kbr <klaus@my.kbrnet.de>
2019-07-14 14:48:58 +0200

    new beta tag v1.2b

--- SDrive.c
+++ SDrive.c
@@ -28,7 +28,7 @@
 #include "tape.h"
 
 #define SWVERSIONMAJOR  1
-#define SWVERSIONMINOR  1
+#define SWVERSIONMINOR  2b
 
 //workaround to get version numbers converted to strings
 #define STR_A(x)        #x
