852caaa662df125314c689549cf66e66cbac23d0
kbr <klaus@my.kbrnet.de>
2022-12-04 16:57:20 +0100

    new beta tag

--- SDrive.c
+++ SDrive.c
@@ -27,7 +27,7 @@
 #include "tape.h"
 
 #define SWVERSIONMAJOR  1
-#define SWVERSIONMINOR  3
+#define SWVERSIONMINOR  4b
 
 //workaround to get version numbers converted to strings
 #define STR_A(x)        #x
