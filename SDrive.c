872e98d55b50c6bf6d78ebfce3197808f2078799
kbr <klaus@my.kbrnet.de>
2025-02-02 15:41:27 +0100

    remove beta tag

--- SDrive.c
+++ SDrive.c
@@ -28,7 +28,7 @@
 #include "tape.h"
 
 #define SWVERSIONMAJOR  1
-#define SWVERSIONMINOR  4b
+#define SWVERSIONMINOR  4
 
 //workaround to get version numbers converted to strings
 #define STR_A(x)        #x
